7c6bccf6945499b9e955b911072cfcde5e95ea4b
Ruben <ruben.cadenas@itland.pe>
2021-05-19 11:09:04 -0500

    FEAT: Added files for task "2. Size validation"

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""Square class with constructor"""
+
+
+class Square:
+
+    """Class constructor.
+    Args:
+        size (int): Size of the square
+    Raises:
+        TypeError: If the size type is not an int
+        ValueError: If size is less than 0
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        
+        self.__size = size
