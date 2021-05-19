9f1b2abbb8c383dc37d9cd26aa1de3b8a1cc1000
Ruben <ruben.cadenas@itland.pe>
2021-05-19 11:28:55 -0500

    FIX: use setter and property

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,15 +6,21 @@ class Square:
 
     """Class constructor."""
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
-    """Setter method for the size property.
+    """size property: returns the value of the __size attribute."""
+    @property
+    def size(self):
+        return self.__size
+
+    """size setter property: assign the __size attribute.
     Args:
         size (int): Size of the square
     Raises:
         TypeError: If the size type is not an int
         ValueError: If size is less than 0
     """
+    @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError('size must be an integer')
